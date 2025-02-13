9c9977feb9ffd777c3e7cde77aa737899cd9c569
Hariprasath.S <94157377+Hariprasath161103@users.noreply.github.com>
2025-02-13 11:38:21 +0530

    Add files via upload

---
+++ gallery.js
@@ -0,0 +1,7 @@
+var img=document.getElementById("images")
+
+function large(event){
+   img.style.width="100px"
+   img.style.transform = "scale(1.5)"
+    img.style.transition = "transform 0.25s ease"
+}
